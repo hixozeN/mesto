6bae0ebd9c0acbaee482b228f5cf16db2359fab1
Vladislav Kolyanichenko <aljkeee@gmail.com>
2022-12-12 22:59:40 +0300

    refactor: removed evtLstnrs from func to glob, del excess code

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,37 @@
-// Popup
-let popup = document.querySelector('.popup');
-let openEditFormButton = document.querySelector('.head-profile__edit-button');
-let closeEditFormButton = document.querySelector('.popup__close-button');
-let formElement = document.querySelector(".popup__form");
+// #Popup
+const popup = document.querySelector('.popup');
+const buttonOpenEditForm = document.querySelector('.head-profile__edit-button');
+const buttonCloseEditForm = document.querySelector('.popup__close-button');
+const formElement = document.querySelector(".popup__form");
 let nameInput = document.querySelector('.popup__input_field_name');
 let jobInput = document.querySelector('.popup__input_field_job');
 let userNameText = document.querySelector('.head-profile__username');
 let userJobText = document.querySelector('.head-profile__job');
 
+/*  Function which will refresh actual input values from username and userjob titles
+    when user will toggle popup */
 function refreshInputValues() {
   nameInput.value = userNameText.textContent;
   jobInput.value = userJobText.textContent;
-  return;
 }
 
-function controlPopup(open, close) {
-  open.addEventListener('click', (e) => {
-    popup.classList.add('popup_opened');
-    refreshInputValues();
-  });
-
-  close.addEventListener('click', (e) => {
-    popup.classList.remove('popup_opened');
-  });
+// This function will control the popups by toggle popup_opened class
+function controlPopup() {
+  popup.classList.toggle('popup_opened');
+  refreshInputValues();
 }
 
+// Listeners for buttons
+buttonOpenEditForm.addEventListener('click', controlPopup);
+buttonCloseEditForm.addEventListener('click', controlPopup);
+
+// Function that handles the form submission
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   userNameText.textContent = nameInput.value;
   userJobText.textContent = jobInput.value;
-
-  popup.classList.remove('popup_opened');
+  controlPopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// Listener for a sumbit event
 formElement.addEventListener('submit', formSubmitHandler);
-
-// и отправлять форму по нажатию на энтер
-let EditProfileSaveBtn = document.querySelector('.popup__save-button');
-
-formElement.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-    event.preventDefault(); // отменяем преезагрузку страницы при отправке формы
-    EditProfileSaveBtn.click(); // отправляем по энтеру
-    popup.classList.remove('popup_opened'); // Закрываем окно после отправки
-  }
-});
-
-controlPopup(openEditFormButton, closeEditFormButton);
