02fbcf09f8627e5c823d2e9a642ba78f4bc7bcc7
Vladislav Kolyanichenko <aljkeee@gmail.com>
2022-12-25 14:43:51 +0300

    feat: cards deletion functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-// #Popups
+// #Constants
+// Popups
 const popupEdit = document.querySelector('.popup_edit');
 const popupAdd = document.querySelector('.popup_add');
-
-// #Buttons
+// Buttons
 const buttonOpenEditForm = document.querySelector('.head-profile__edit-button');
 const buttonAdd = document.querySelector('.head-profile__add-button');
 const buttonClose = document.querySelectorAll('.popup__close-button');
-
-// #Constants
+// Edit form for user profile (username & userjob)
 const formEditProfile = document.querySelector('.popup__form_type_profile-edit');
+// Consts for adding and creating card
 const formAddCard = document.querySelector('.popup__form_type_card-add');
 const cardsContainer = document.querySelector('.photo-feed');
 const template = document.querySelector('#card-template').content;
 const cardTitleInput = document.querySelector('.popup__input_field_placename');
 const cardImageInput = document.querySelector('.popup__input_field_placeurl');
-// Cards at photo-feed
+// Cards at photo-feed (preload)
 const initialCards = [
   {
     name: 'Рабочее место',
@@ -42,15 +42,14 @@ const initialCards = [
   }
 ];
 
-// #Variables
+// #Global variables
 let nameInput = document.querySelector('.popup__input_field_name');
 let jobInput = document.querySelector('.popup__input_field_job');
 let userNameText = document.querySelector('.head-profile__username');
 let userJobText = document.querySelector('.head-profile__job');
 
 // #Functions
-/* This function will control the popups by toggle popup_opened class
-   and refresh inputs' value, when popup opens */
+/* This function will control the popups by toggle popup_opened class */
 function controlPopup(option) {
   if (option.closest('.popup').classList.contains('popup_opened')) {
     option.closest('.popup').classList.remove('popup_opened');
@@ -67,35 +66,33 @@ function formSubmitHandler(evt) {
   controlPopup(evt.target);
 }
 
+// Function that handles the add form submission
 function formAddCardHandler(evt) {
   evt.preventDefault();
   cardPrepend(cardCreate(cardTitleInput.value, cardImageInput.value));
   controlPopup(evt.target);
 }
 
+// Function that creates new cards
 function cardCreate(name, link) {
-  // for (let i = 0; i < initialCards.length; i++) {
-  //   cardsContainer.insertAdjacentHTML('beforeend',
-  //     `<article class="card">
-  //     <img class="card__image" src="${initialCards[i].link}" alt="${initialCards[i].name}">
-  //     <div class="card__heading">
-  //       <h2 class="card__title">${initialCards[i].name}</h2>
-  //       <button class="card__like-button" type="button" aria-label="Нравится"></button>
-  //     </div>
-  //   </article>`);
-  // };
-  //// Второй вариант.
-  const newCard = template.querySelector('.card').cloneNode(true);
+  const newCard = template.querySelector('.card').cloneNode(true); // choose div from template and clone it
   const cardImg = newCard.querySelector('.card__image');
   const cardTitle = newCard.querySelector('.card__title');
   const cardLikeButton = newCard.querySelector('.card__like-button');
+  const cardDeleteButton = newCard.querySelector('.card__delete-button');
 
-  cardImg.alt = name;
+  cardImg.alt = name; // Image's alt attribute will equal card's title
   cardImg.src = link;
   cardTitle.textContent = name;
 
+  // Listener for card like button
   cardLikeButton.addEventListener('click', function (evt) {
     evt.target.classList.toggle('card__like-button_active');
+  });
+
+  // Listener for card delete button
+  cardDeleteButton.addEventListener('click', function (evt) {
+    newCard.remove();
   });
 
   return newCard;
@@ -106,30 +103,26 @@ function cardPrepend(card) {
 }
 
 // #Listeners
-// Listeners for form buttons
+// Listener for edit form button
 buttonOpenEditForm.addEventListener('click', () => {
   controlPopup(popupEdit);
   nameInput.value = userNameText.textContent;
   jobInput.value = userJobText.textContent;
 });
-buttonAdd.addEventListener('click', () => controlPopup(popupAdd));
+
+// Listener for add form button
+buttonAdd.addEventListener('click', () => {
+  cardTitleInput.value = "";
+  cardImageInput.value = "";
+  controlPopup(popupAdd);
+});
 
 // Listener for each button which closes popup
 buttonClose.forEach(function (button) {
   button.addEventListener('click', () => controlPopup(button));
 });
 
-// Like button listener
-// document.addEventListener('DOMContentLoaded', function () {
-//   const buttonLike = document.querySelectorAll('.card__like-button');
-//   buttonLike.forEach(function (button) {
-//     button.addEventListener('click', () => {
-//       button.classList.toggle('card__like-button_active');
-//     });
-//   });
-// });
-
-// Listener for a sumbit event
+// Listeners for a sumbit events
 formEditProfile.addEventListener('submit', formSubmitHandler);
 formAddCard.addEventListener('submit', formAddCardHandler);
 
